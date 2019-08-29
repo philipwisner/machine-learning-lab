2830c7b7d967c389722a8f6c7986d38aa10a25b0
Philip Wisner <philipwisner@gmail.com>
2019-08-29 14:41:17 -0400

    added router links and base styling

--- src/components/NavPanel.vue
+++ src/components/NavPanel.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="nav-panel">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <img class="logo" src="../assets/logo.svg" alt="logo">
+    <div class="title-container">
+      <h1 class="title-name">Machine Learning Lab</h1>
+      <p class="title-subtext">Welcome to Jefferson’s Machine Learning Playground</p>
+
+    </div>
+
+    <router-link to="/age">Age</router-link> |
+    <router-link to="/vision">Vision</router-link> |
+    <router-link to="/life">Life</router-link> |
+    <router-link to="/recommend">Recommend</router-link>
   </div>
 </template>
 
@@ -13,7 +22,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-a {
+.nav-panel {
+  padding: 5%;
+}
+.logo {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+}
+.title-container {
+  border: 1px solid white;
+  margin-top: 40px;
+}
+.title-name {
   color: white;
+  font-size: 38px;
+}
+.title-subtext {
+  color: #B4D9FF;
+  font-size: 18px;
 }
 </style>
