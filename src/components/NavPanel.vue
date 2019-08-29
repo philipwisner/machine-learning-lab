59d05167524c3f96b5d369ad1d666e42614133ef
Philip Wisner <philipwisner@gmail.com>
2019-08-29 14:26:17 -0400

    routing is in navpanel

---
+++ src/components/NavPanel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav-panel">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavPanel',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: white;
+}
+</style>
