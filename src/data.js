6b16071e33b6882b0d451d23d6c6135243327c04
Philip Wisner <philipwisner@gmail.com>
2019-08-29 23:29:46 -0400

    extracted data out

---
+++ src/data.js
@@ -0,0 +1,51 @@
+const appData = {};
+
+appData.categories = [
+  {
+    name: "Computer Vision",
+    description: "Model that predicts a person’s age based off an uploaded photo",
+    link: `/vision`,
+    logo: "Eye"
+  },
+  {
+    name: "NLP/NLU",
+    description: "Model that predicts a person’s age based off an uploaded photo",
+    link: `/nlp`,
+    logo: "Graph"
+  },
+  {
+    name: "GAN",
+    description: "Model that predicts a person’s age based off an uploaded photo",
+    link: `/gan`,
+    logo: "Person"
+  },
+  {
+    name: "Tabular",
+    description: "Model that predicts a person’s age based off an uploaded photo",
+    link: `/tabular`,
+    logo: "Thumb"
+  },
+  {
+    name: "Healthcare",
+    description: "Model that predicts a person’s age based off an uploaded photo",
+    link: `/healthcare`,
+    logo: "Thumb"
+  }
+];
+
+appData.reasons = [
+  {
+    heading: 'Sample Reason 1',
+    content: 'Ut iaculis ligula mauris, ac porttitor tellus vehicula semper.'
+  },
+  {
+    heading: 'Sample Reason 2',
+    content: 'Ut iaculis ligula mauris, ac porttitor tellus vehicula semper.'
+  },
+  {
+    heading: 'Sample Reason 3',
+    content: 'Ut iaculis ligula mauris, ac porttitor tellus vehicula semper.'
+  }
+]
+
+module.exports = appData;
