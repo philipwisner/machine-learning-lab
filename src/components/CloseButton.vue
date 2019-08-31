cf6d3cff7d52d77393eff8e4c1c5a94651476768
Philip Wisner <philipwisner@gmail.com>
2019-08-31 08:53:49 -0400

    created close component

---
+++ src/components/CloseButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="close-button">
+    <button @click="goToHome">
+      <img src="../assets/close.svg" alt="close">
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CloseButton',
+  methods: {
+    goToHome() {
+      this.$router.push('/');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  border: none;
+  background: none;
+  position: absolute;
+  right: 30px;
+  cursor: pointer;
+}
+button:focus {
+  outline: none;
+}
+img {
+  width: 25px;
+}
+
+</style>
