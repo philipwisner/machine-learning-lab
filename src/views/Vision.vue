f1b9a9a93222e60ba57d5702f77a1e6fea98ae8a
Philip Wisner <philipwisner@gmail.com>
2019-08-31 08:54:39 -0400

    extracted out app

--- src/views/Vision.vue
+++ src/views/Vision.vue
@@ -1,25 +1,22 @@
 <template>
   <div class="vision-container">
-    <router-link to="/" class="close">x</router-link>
-    <div class="computer-vision">
-      <h1>Computer Vision</h1>
-      <h3>Classify Bear Images 🐻</h3>
-      <p class="description">Use images of teddy bears, black bears, grizzly bears, or all three!</p>
-      <p>Image upload</p>
-      <input type="file" name="pic" accept="image/*" @change="imageUploaded">
-      <p>Image preview</p>
-      <p>{{picturePreview}}</p>
-      <p>RESULT: {{pictureResult}}</p>
-      <button @click="analyzeImage">Analyze</button>
-    </div>
+    <CloseButton/>
+    <h1>Computer Vision</h1>
   </div>
 </template>
 
-</form>
 
 <script>
+import CloseButton from '@/components/CloseButton.vue'
+import BearImages from '@/components/Apps/BearImages.vue'
+
+
 export default {
   name: 'Vision',
+    components: {
+    CloseButton,
+    BearImages
+  },
   data() {
     return {
       pictureResult: '',
@@ -41,21 +38,13 @@ export default {
 </script>
 
 <style scoped>
-.close {
-  position: absolute;
-  top: 40px;
-  right: 40px;
-  text-decoration: none;
-  font-size: 20px;
-  color: grey;
-}
 h1 {
   margin-top: 20px;
   margin-bottom: 20px;
-  color: #0099FF;
+  color: #34D671;
 }
 h3 {
-  color: #0099FF;
+  color: #34D671;
   margin-bottom: 20px;
 }
 .computer-vision {
@@ -63,15 +52,5 @@ h3 {
 }
 .description {
   margin-bottom: 30px;
-}
-button {
-  margin-top: 20px;
-  color: white;
-  background: #0099FF;
-  border: none;
-  border-radius: 3px;
-  padding: 5px 20px;
-  font-weight: 700;
-  font-size: 14px;
 }
 </style>
