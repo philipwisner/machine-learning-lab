d87c6267ed00a9ce5fe1b7065554594da0644b04
Philip Wisner <philipwisner@gmail.com>
2019-09-03 16:14:09 -0400

    added headers

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from "axios"
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 import VueAxios from "vue-axios"
 
 Vue.use(VueAxios, axios);
