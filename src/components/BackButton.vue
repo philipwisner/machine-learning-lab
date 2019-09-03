578d73ca0bc41b4a123280a8e164c704d4bb231d
Philip Wisner <philipwisner@gmail.com>
2019-09-03 11:04:06 -0400

    added back button

---
+++ src/components/BackButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="back-button">
+    <button @click="goBack(path)">
+      <img src="../assets/back.svg" alt="back">
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackButton',
+  props: {
+    path: String
+  },
+  methods: {
+    goBack(path) {
+      this.$router.push(path);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+button {
+  float: left;
+  border: none;
+  background: none;
+  cursor: pointer;
+}
+button:focus {
+  outline: none;
+}
+img {
+  width: 25px;
+}
+
+</style>
