4aa3bc6fc65c116ca97516eba3c99ef803517434
Philip Wisner <philipwisner@gmail.com>
2019-09-04 10:06:05 -0400

    Abstracted out data

--- src/data.js
+++ src/data.js
@@ -3,43 +3,123 @@ const appData = {};
 appData.categories = [
   {
     name: "Computer Vision",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sapien quam, efficitur sed nibh tempus.",
+    description: `Allows computers to "see" and understand the content of images and photographs`,
+    longDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sapien quam, efficitur sed nibh tempus, efficitur iaculis lacus. Curabitur ut hendrerit velit. Ut iaculis ligula mauris, ac porttitor tellus vehicula semper. Nam vitae risus lectus. Nam pellentesque sit amet dui sit amet vehicula.",
     link: "/vision",
     logo: "Eye",
-    class: "vision"
+    class: "vision",
+    apps: [
+      {
+        name: "Bear Image",
+        link: "vision/bearimages"
+      },
+      {
+        name: "Sample App 2",
+        link: "vision/"
+      },
+      {
+        name: "Sample App 3",
+        link: "vision/"
+      }
+    ]
   },
   {
     name: "NLP/NLU",
+    fullName: "Natural Language Processing/Understanding",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sapien quam, efficitur sed nibh tempus.",
+      "Allows computers to understand, analyze, manipulate and potentially generate human language.",
+    longDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sapien quam, efficitur sed nibh tempus, efficitur iaculis lacus. Curabitur ut hendrerit velit. Ut iaculis ligula mauris, ac porttitor tellus vehicula semper. Nam vitae risus lectus. Nam pellentesque sit amet dui sit amet vehicula.",
     link: "/nlp",
     logo: "Graph",
-    class: "nlp"
+    class: "nlp",
+    apps: [
+      {
+        name: "NLP Child App",
+        link: "nlp/nlpchild"
+      },
+      {
+        name: "Sample App 2",
+        link: "nlp/"
+      },
+      {
+        name: "Sample App 3",
+        link: "nlp/"
+      }
+    ]
   },
   {
     name: "GAN",
+    fullName: "General Adversarial Network",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sapien quam, efficitur sed nibh tempus.",
+      "Two neural networks compete with each other to become more accurate in their predictions.",
+    longDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sapien quam, efficitur sed nibh tempus, efficitur iaculis lacus. Curabitur ut hendrerit velit. Ut iaculis ligula mauris, ac porttitor tellus vehicula semper. Nam vitae risus lectus. Nam pellentesque sit amet dui sit amet vehicula.",
     link: "/gan",
     logo: "Person",
-    class: "gan"
+    class: "gan",
+    apps: [
+      {
+        name: "GAN Child App",
+        link: "gan/ganchild"
+      },
+      {
+        name: "Sample App 2",
+        link: "gan/"
+      },
+      {
+        name: "Sample App 3",
+        link: "gan/"
+      }
+    ]
   },
   {
     name: "Tabular",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sapien quam, efficitur sed nibh tempus.",
+    description: "Train models using traditional table based data sets.",
+    longDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sapien quam, efficitur sed nibh tempus, efficitur iaculis lacus. Curabitur ut hendrerit velit. Ut iaculis ligula mauris, ac porttitor tellus vehicula semper. Nam vitae risus lectus. Nam pellentesque sit amet dui sit amet vehicula.",
     link: "/tabular",
     logo: "Thumb",
-    class: "tabular"
+    class: "tabular",
+    apps: [
+      {
+        name: "Tabular Child App",
+        link: "tabular/tabularchild"
+      },
+      {
+        name: "Sample App 2",
+        link: "tabular/"
+      },
+      {
+        name: "Sample App 3",
+        link: "tabular/"
+      }
+    ]
   },
   {
     name: "Healthcare",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sapien quam, efficitur sed nibh tempus.",
+      "Machine Learning and AI are transforming the healthcare industry.",
+    longDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sapien quam, efficitur sed nibh tempus, efficitur iaculis lacus. Curabitur ut hendrerit velit. Ut iaculis ligula mauris, ac porttitor tellus vehicula semper. Nam vitae risus lectus. Nam pellentesque sit amet dui sit amet vehicula.",
     link: "/healthcare",
     logo: "Thumb",
-    class: "healthcare"
+    class: "healthcare",
+    apps: [
+      {
+        name: "Healthcare Child App",
+        link: "healthcare/healthcarechild"
+      },
+      {
+        name: "Sample App 2",
+        link: "healthcare/"
+      },
+      {
+        name: "Sample App 3",
+        link: "healthcare/"
+      }
+    ]
   }
 ];
 
