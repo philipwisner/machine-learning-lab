e2536220e255a1e5c7219f0d825779850e2137eb
Philip Wisner <philipwisner@gmail.com>
2019-09-05 12:28:43 -0400

    create sample images for apps

---
+++ src/components/Reusable/SampleImages.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="sample-images">
+    <h3>Sample Images</h3>
+    <div class="sample-images-container">
+      <div class="image-container" :key="image.$index" v-for="image in selectedImages" :style="{ backgroundImage: 'url(' + image.path + ')' }" @click="setSelectedFile(image)"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SampleImages',
+  props: {
+    selectedImages: Array,
+  },
+  data() {
+    return {
+      //Detemine image set based off url?
+    }
+  },
+  methods: {
+    setSelectedFile(image) {
+      console.log('image selected', image);
+      this.$emit('image-selected', image);
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin-top: 15px;
+}
+.sample-images-container {
+  margin: 10px 0 20px 0;
+  display: flex;
+}
+.image-container {
+  cursor: pointer;
+  width: 70px;
+  height: 70px;
+  margin-right:10px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+</style>
